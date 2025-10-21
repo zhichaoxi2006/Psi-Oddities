e0f4fb4e9b737d17bcf27c2003baf71784fdf72e
ZiG60020405 <47026578+zhichaoxi2006@users.noreply.github.com>
2025-10-21 19:59:32 +0800

    add tooltip for Psimetal Shield

--- src/main/java/com/zhichaoxi/psi_oddities/item/ItemPsimetalShield.java
+++ src/main/java/com/zhichaoxi/psi_oddities/item/ItemPsimetalShield.java
@@ -3,17 +3,23 @@ package com.zhichaoxi.psi_oddities.item;
 import net.fabric_extras.shield_api.item.CustomShieldItem;
 import net.minecraft.core.Holder;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.util.Tuple;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.neoforge.capabilities.ICapabilityProvider;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import vazkii.psi.api.cad.ISocketable;
+import vazkii.psi.api.internal.TooltipHelper;
 import vazkii.psi.common.item.tool.IPsimetalTool;
 
 import java.util.List;
@@ -49,5 +55,15 @@ public class ItemPsimetalShield extends CustomShieldItem implements IPsimetalToo
     @Override
     public ICapabilityProvider initCapabilities(ItemStack stack, @Nullable CompoundTag nbt) {
         return IPsimetalTool.super.initCapabilities(stack, nbt);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void appendHoverText(@NotNull ItemStack stack, @Nullable TooltipContext context, List<Component> tooltip, @NotNull TooltipFlag advanced) {
+        TooltipHelper.tooltipIfShift(tooltip, () -> {
+            Component componentName = ISocketable.getSocketedItemName(stack, "psimisc.none");
+            tooltip.add(Component.translatable("psimisc.spell_selected", componentName));
+            tooltip.add(Component.translatable("psi_oddities.event.blocked"));
+        });
     }
 }
