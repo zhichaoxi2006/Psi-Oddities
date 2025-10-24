70fb4c0c1397727df1cc5399fc79a99664788772
ZiG60020405 <47026578+zhichaoxi2006@users.noreply.github.com>
2025-10-24 13:08:47 +0800

    some change

--- src/main/java/com/zhichaoxi/psi_oddities/spell/param/ParamString.java
+++ src/main/java/com/zhichaoxi/psi_oddities/spell/param/ParamString.java
@@ -1,6 +1,5 @@
 package com.zhichaoxi.psi_oddities.spell.param;
 
-import vazkii.psi.api.spell.Spell;
 import vazkii.psi.api.spell.param.ParamSpecific;
 
 public class ParamString extends ParamSpecific<String> {
